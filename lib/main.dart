b91c46e801fcc7bb4990c08853f8902b41d765bc
Maina Snr <sharkeemaina@gmail.com>
2025-10-06 10:33:23 +0300

    changes

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:fauna_pulse/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:fauna_pulse/config/themes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+/// MyApp widget
+/// This widget is the root of the application.
+/// It sets up the MaterialApp with a title, theme, and home screen.
+/// The home screen is the SplashScreen.
+/// The app uses a custom theme defined in the AppTheme class.
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0anZvZWR1cmRud21wb3FseWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDg1NTgsImV4cCI6MjA3NTEyNDU1OH0.EKJuoOw7R0vHY9q-H0DKqWJHNSxA5FOpZutwN_p0t-k",
+    url: 'https://ttjvoedurdnwmpoqlyjl.supabase.co',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FaunaPulse',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.themeData,
+      home: const SplashScreen(),
+    );
+  }
+}
